5c8b70923a23f4643742cb1e972fecf988280c81
Aleksandr Gavrilov <alg@digt.ru>
2016-05-27 15:29:41 +0300

    Fix: test for build and verify chain

--- test/chain.js
+++ test/chain.js
@@ -5,57 +5,98 @@ var DEFAULT_CERTSTORE_PATH = "test/CertStore";
 
 describe('Chain', function () {
     var store;
-	var providerSystem;
+    var providerSystem;
     var chain;
     var outChain;
+    var rv;
 
     it('init', function () {
         chain = new trusted.pki.Chain();
         assert.equal(chain != null, true);
-        
+
         providerSystem = new trusted.pkistore.Provider_System(DEFAULT_CERTSTORE_PATH);
         assert.equal(providerSystem != null, true);
-        
+
         store = new trusted.pkistore.PkiStore(DEFAULT_CERTSTORE_PATH + "/cash.json");
         assert.equal(store != null, true);
         
-        store.addProvider(providerSystem.handle);	
+        rv = new trusted.pki.Revocation();
+        assert.equal(rv != null, true);
+
+        store.addProvider(providerSystem.handle);
     })
 
     it('build', function () {
         var certs = new trusted.pki.CertificateCollection();
         assert.equal(certs != null, true);
-        
+
         var cert1 = trusted.pki.Certificate.load("test/test-ru.crt", trusted.DataFormat.DER);
         assert.equal(cert1 != null, true);
         certs.push(cert1);
-        
+
         var cert2 = trusted.pki.Certificate.load("test/cert1.crt", trusted.DataFormat.PEM);
         assert.equal(cert2 != null, true);
         certs.push(cert2);
-        
+
         var cert3 = trusted.pki.Certificate.load("test/test.crt", trusted.DataFormat.DER);
         assert.equal(cert3 != null, true);
         certs.push(cert3);
-        
+
         var cert4 = trusted.pki.Certificate.load("test/test2.crt", trusted.DataFormat.PEM);
         assert.equal(cert4 != null, true);
         certs.push(cert4);
-        
+
         assert.equal(certs.length == 4, true);
-        
-        outChain = chain.buildChain(cert3, certs); 
-        
+
+        outChain = chain.buildChain(cert3, certs);
+
         assert.equal(outChain.length == 2, true);
-    }); 
-    
+    });
+
     it('verify', function () {
         var crl = trusted.pki.Crl.load("test/sfsca.crl");
         store.addCrl(providerSystem.handle, "CRL", crl, 0);
         providerSystem = new trusted.pkistore.Provider_System(DEFAULT_CERTSTORE_PATH);
         assert.equal(providerSystem != null, true);
         store.addProvider(providerSystem.handle);
-         
-        assert.equal(chain.verifyChain(outChain, store) === true, true);
-    })
+
+        var crls = new trusted.pki.CrlCollection();
+        var rv = new trusted.pki.Revocation();
+        for (var i = 0; i < outChain.length; i++) {
+            var tmpCrl = rv.getCrlLocal(outChain.items(i), store);
+            if (tmpCrl && rv.checkCrlTime(tmpCrl)) {
+                crls.push(tmpCrl)
+            }
+        }
+
+        assert.equal(chain.verifyChain(outChain, crls) === true, true);
+    });
+    
+    it('download CRL',  function (done) { 
+       this.timeout(10000);        
+       var testCert = trusted.pki.Certificate.load("test/test-ru.crt", trusted.DataFormat.DER);
+       assert.equal(testCert != null, true);
+       
+       var pathForSave = DEFAULT_CERTSTORE_PATH + "/temp.crl";
+       var crl;
+          
+       var distPoints = rv.getCrlDistPoints(testCert);
+       assert.equal(distPoints.length === 5, true);
+       
+       rv.downloadCRL(distPoints, pathForSave, function(err, res){
+           if (err){
+               console.log(err);
+               done(err);
+           }
+           else{
+                crl = res;
+                console.log("All CRL downloaded");
+                console.log("Isuer name:", crl.issuerFriendlyName);
+                
+                store.addCrl(providerSystem.handle, "CRL", crl, 0); 
+                
+                done();
+           }          
+        });                             
+  });
 });
--- test/store.js
+++ test/store.js
@@ -5,57 +5,57 @@ var async = require("async");
 var DEFAULT_CERTSTORE_PATH = "test/CertStore";
 
 describe('Store', function () {
-	var store;
-	var providerSystem, providerMicrosoft;
+    var store;
+    var providerSystem, providerMicrosoft;
     var certWithKey;
-    
+
     var uri;
-    
-	it('init', function () {
+
+    it('init', function () {
         providerSystem = new trusted.pkistore.Provider_System(DEFAULT_CERTSTORE_PATH);
         assert.equal(providerSystem != null, true);
-       	
-		store = new trusted.pkistore.PkiStore(DEFAULT_CERTSTORE_PATH+"/cash.json");
-        assert.equal(store != null, true);	
+
+        store = new trusted.pkistore.PkiStore(DEFAULT_CERTSTORE_PATH + "/cash.json");
+        assert.equal(store != null, true);
     })
-    
-    it('add pki objects', function () {       	
-       var cert  = trusted.pki.Certificate.load("test/cert1.crt", trusted.DataFormat.PEM);
-       var crl = trusted.pki.Crl.load("test/test.crl");
-       var key = trusted.pki.Key.privkeyLoad("test/cert1.key", trusted.DataFormat.PEM, "");
-       var csr = trusted.pki.CertificationRequest.load("test/test.csr", trusted.DataFormat.PEM, "");
-       
-       uri = store.addCert(providerSystem.handle, "MY", cert, 0);
-       store.addCrl(providerSystem.handle, "CRL", crl, 0);
-       store.addKey(providerSystem.handle, key, "");
-       store.addCsr(providerSystem.handle, "MY", csr);
-       
-       var newCert = trusted.pki.Certificate.load("test/CertStore/MY/84cd1d796cfb42d00166737c6e16d596cf83695e_15b5c91c943cd687ccf6b85a7b28273f281d3eba.crt", trusted.DataFormat.PEM);
-       assert.equal(cert.thumbprint == newCert.thumbprint, true);
-       
-       var newCrl = trusted.pki.Crl.load("test/CertStore/CRL/1ebb0526075855661c09d7d9b59abd950bdae0ef.crl");
-       assert.equal(crl.thumbprint == newCrl.thumbprint, true);
-       
-       var newKey = trusted.pki.Key.privkeyLoad("test/CertStore/MY/15b5c91c943cd687ccf6b85a7b28273f281d3eba.key", trusted.DataFormat.PEM, "");
-       assert.equal(newKey != null, true);
+
+    it('add pki objects', function () {
+        var cert = trusted.pki.Certificate.load("test/cert1.crt", trusted.DataFormat.PEM);
+        var crl = trusted.pki.Crl.load("test/test.crl");
+        var key = trusted.pki.Key.privkeyLoad("test/cert1.key", trusted.DataFormat.PEM, "");
+        var csr = trusted.pki.CertificationRequest.load("test/test.csr", trusted.DataFormat.PEM, "");
+
+        uri = store.addCert(providerSystem.handle, "MY", cert, 0);
+        store.addCrl(providerSystem.handle, "CRL", crl, 0);
+        store.addKey(providerSystem.handle, key, "");
+        store.addCsr(providerSystem.handle, "MY", csr);
+
+        var newCert = trusted.pki.Certificate.load("test/CertStore/MY/84cd1d796cfb42d00166737c6e16d596cf83695e_15b5c91c943cd687ccf6b85a7b28273f281d3eba.crt", trusted.DataFormat.PEM);
+        assert.equal(cert.thumbprint == newCert.thumbprint, true);
+
+        var newCrl = trusted.pki.Crl.load("test/CertStore/CRL/1ebb0526075855661c09d7d9b59abd950bdae0ef.crl");
+        assert.equal(crl.thumbprint == newCrl.thumbprint, true);
+
+        var newKey = trusted.pki.Key.privkeyLoad("test/CertStore/MY/15b5c91c943cd687ccf6b85a7b28273f281d3eba.key", trusted.DataFormat.PEM, "");
+        assert.equal(newKey != null, true);
     })
-    
-    it('add providers', function () {	
+
+    it('add providers', function () {
         providerSystem = new trusted.pkistore.Provider_System(DEFAULT_CERTSTORE_PATH);
         assert.equal(providerSystem != null, true);
         store.addProvider(providerSystem.handle);
-        
+
         providerMicrosoft = new trusted.pkistore.ProviderMicrosoft();
         assert.equal(providerMicrosoft != null, true);
-        store.addProvider(providerMicrosoft.handle);	
+        store.addProvider(providerMicrosoft.handle);
     })
-	
-	it('find', function () {	
-       var certs = store.find({
+
+    it('find', function () {
+        var certs = store.find({
             type: ["CERTIFICATE"],
-            category: ["MY"]        
+            category: ["MY"]
         });
-        
+
         for (var i = 0; i < certs.length; i++) {
             var item = certs[i];
             if (item.key) {
@@ -67,54 +67,30 @@ describe('Store', function () {
                 assert.equal(cert.subjectName.length > 0, true);
             }
         }
-        
+
     })
-    
-    it('findKey', function () {	
-       var key = store.findKey({
+
+    it('findKey', function () {
+        var key = store.findKey({
             type: ["CERTIFICATE"],
             provider: ["SYSTEM"],
             category: ["MY"],
             hash: certWithKey.thumbprint.toString('hex')
         });
-        
-        assert.equal(!!key, true, "Отсутствует ключ для сертификата");     
-    })
-    
-    it('json', function () {	
-       var items = store.find();
-       store.cash.import(items);
-       var exportPKI = store.cash.export();
-       assert.equal(exportPKI.length > 0, true);
-    })
-       
-    it('download CRL',  function (done) { 
-       this.timeout(10000);        
-       var testCert = trusted.pki.Certificate.load("test/test-ru.crt", trusted.DataFormat.DER);
-       var path = DEFAULT_CERTSTORE_PATH + "/temp.crl";
-       var crl;
-           
 
-       store.downloadCRL(testCert, path, function(err, res){
-           if (err){
-               console.log(err);
-               done(err);
-           }
-           else{
-                crl = res;
-                console.log("All CRL downloaded");
-                console.log("Isuer name:", crl.issuerFriendlyName);
-                
-                store.addCrl(providerSystem.handle, "CRL", crl, 0); 
-                
-                done();
-           }          
-        });                             
-  });
-  
-  it('Object to PkiItem', function () {	
-       var item = providerSystem.objectToPkiItem(uri);
-       assert.equal(item != null, true);
+        assert.equal(!!key, true, "Отсутствует ключ для сертификата");
     })
-     	
+
+    it('json', function () {
+        var items = store.find();
+        store.cash.import(items);
+        var exportPKI = store.cash.export();
+        assert.equal(exportPKI.length > 0, true);
+    })
+
+    it('Object to PkiItem', function () {
+        var item = providerSystem.objectToPkiItem(uri);
+        assert.equal(item != null, true);
+    })
+
 });
