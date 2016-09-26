218dd5cda2751c2eb3e9dfabe95f5455f31a9ef0
Aleksandr Gavrilov <alg@digt.ru>
2016-09-26 12:03:49 +0300

    Add test search recipient cert in store

--- test/cipher.js
+++ test/cipher.js
@@ -4,6 +4,7 @@ var assert = require("assert");
 var fs = require("fs");
 var trusted = require("../index.js");
 
+var DEFAULT_CERTSTORE_PATH = "test/CertStore";
 var DEFAULT_RESOURCES_PATH = "test/resources";
 var DEFAULT_OUT_PATH = "test/out";
 
@@ -39,6 +40,8 @@ describe("CipherSYMMETRIC", function() {
 
 describe("CipherASSYMETRIC", function() {
     var cipher;
+    var ris, ri;
+    var store, cert, key;
 
     it("init", function() {
         cipher = new trusted.pki.Cipher();
@@ -62,7 +65,7 @@ describe("CipherASSYMETRIC", function() {
     });
 
     it("recipient cert", function() {
-        var cert = new trusted.pki.Certificate();
+        cert = new trusted.pki.Certificate();
 
         assert.equal(cert !== null, true);
         cert.load(DEFAULT_RESOURCES_PATH + "/cert1.crt", trusted.DataFormat.PEM);
@@ -71,7 +74,7 @@ describe("CipherASSYMETRIC", function() {
     });
 
     it("recipient private key", function() {
-        var key = new trusted.pki.Key();
+        key = new trusted.pki.Key();
 
         assert.equal(key !== null, true);
         key.readPrivateKey(DEFAULT_RESOURCES_PATH + "/cert1.key", trusted.DataFormat.PEM, "");
@@ -79,8 +82,6 @@ describe("CipherASSYMETRIC", function() {
     });
 
     it("recipient info", function() {
-        var ris, ri;
-
         ris = cipher.getRecipientInfos(DEFAULT_OUT_PATH + "/encAssym.txt", trusted.DataFormat.PEM);
         assert.equal(ris.length, 2, "Recipients length 2");
 
@@ -97,7 +98,61 @@ describe("CipherASSYMETRIC", function() {
         assert.equal(ri.ktriCertCmp(trusted.pki.Certificate.load(DEFAULT_RESOURCES_PATH + "/test.crt", trusted.DataFormat.DER)) === 0, true, "Compare recipient cert");
     });
 
+    it("find recipient in store", function() {
+        var providerSystem;
+        var item;
+        var certWithKey;
+
+        ri = ris.items(0);
+
+        store = new trusted.pkistore.PkiStore(DEFAULT_CERTSTORE_PATH + "/cash.json");
+        assert.equal(store !== null, true);
+
+        providerSystem = new trusted.pkistore.Provider_System(DEFAULT_CERTSTORE_PATH);
+
+        cert = trusted.pki.Certificate.load(DEFAULT_RESOURCES_PATH + "/cert1.crt", trusted.DataFormat.PEM);
+        key = trusted.pki.Key.readPrivateKey(DEFAULT_RESOURCES_PATH + "/cert1.key", trusted.DataFormat.PEM, "");
+
+        store.addCert(providerSystem.handle, "MY", cert, 0);
+        store.addKey(providerSystem.handle, key, "");
+
+        providerSystem = new trusted.pkistore.Provider_System(DEFAULT_CERTSTORE_PATH);
+        assert.equal(providerSystem !== null, true);
+        store.addProvider(providerSystem.handle);
+
+        var certs = store.find({
+            serial: ri.serialNumber
+        });
+
+        for (var i = 0; i < certs.length; i++) {
+            item = certs[i];
+            if (item.key) {
+                certWithKey = store.getItem(item);
+            }
+            assert.equal(item.type, "CERTIFICATE");
+            if (item.type === "CERTIFICATE") {
+                cert = store.getItem(item);
+                assert.equal(cert.subjectName.length > 0, true);
+            }
+        }
+
+        assert.equal(!!certWithKey, true, "Error get certificate with key");
+
+        key = store.findKey({
+            type: ["CERTIFICATE"],
+            provider: ["SYSTEM"],
+            category: ["MY"],
+            hash: certWithKey.thumbprint.toString("hex")
+        });
+
+        assert.equal(!!key, true, "Error get private key");
+    });
+
     it("decrypt", function() {
+        cipher = new trusted.pki.Cipher();
+        cipher.recipientCert = cert;
+        cipher.privKey = store.getItem(key);
+
         cipher.decrypt(DEFAULT_OUT_PATH + "/encAssym.txt", DEFAULT_OUT_PATH + "/decAssym.txt", trusted.DataFormat.PEM);
     });
 });
