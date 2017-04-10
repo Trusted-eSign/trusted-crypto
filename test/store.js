6441e2e51ea1977db086678d4d9b4892cca4a1e6
Aleksandr Gavrilov <alg@digt.ru>
2017-04-10 16:24:52 +0300

    test: update test certificate

--- test/store.js
+++ test/store.js
@@ -59,7 +59,7 @@ describe("Store", function() {
         store.addKey(providerSystem.handle, key, "");
         store.addCsr(providerSystem.handle, "MY", csr);
 
-        newCert = trusted.pki.Certificate.load(DEFAULT_CERTSTORE_PATH + "/MY/6ad21b07c46b71873da9092aa74706379f0b4ee1_3d7b3ab9cda89acca15457e9c656d5d40f59086d.crt", trusted.DataFormat.PEM);
+        newCert = trusted.pki.Certificate.load(DEFAULT_CERTSTORE_PATH + "/MY/8d270fd25fcf19d0e26d0f045767ca00304c7497_809a28508dae89b0a38a5ae6d059feb9b256b7b2.crt", trusted.DataFormat.PEM);
         assert.equal(cert.thumbprint === newCert.thumbprint, true);
 
         newCrl = trusted.pki.Crl.load(DEFAULT_CERTSTORE_PATH + "/CRL/1ebb0526075855661c09d7d9b59abd950bdae0ef.crl");
